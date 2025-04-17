8e727b523ae9d555320ebaf59864b08d56ecb6bd
Eranosyan Eduard <EranosyanEduard@yandex.ru>
2025-04-17 17:21:02 +0300

    vuetify issue example

---
+++ vuetify2-issue/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, watch } from "vue";
+import { VApp, VBtn, VCard, VDialog } from "vuetify/lib";
+
+const showDialog = ref(false);
+const totalOverlay = ref(0);
+
+watch(showDialog, (next) => {
+  if (!next) {
+    totalOverlay.value = document.querySelectorAll(".v-overlay").length;
+  }
+});
+</script>
+
+<template>
+  <VApp>
+    <header>
+      <h1>VDialog bug: totalOverlay {{ totalOverlay }}</h1>
+    </header>
+    <main>
+      <VBtn type="button" @click="showDialog = true">open dialog</VBtn>
+      <VDialog v-if="showDialog" :value="true">
+        <VCard>
+          <VBtn type="button" @click="showDialog = false">close dialog</VBtn>
+        </VCard>
+      </VDialog>
+    </main>
+  </VApp>
+</template>
---
+++ vuetify2-issue/src/main.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import App from "./App.vue";
+import Vuetify from "vuetify";
+
+Vue.use(Vuetify);
+
+new Vue({
+  vuetify: new Vuetify(),
+  render: (h) => h(App),
+}).$mount("#app");
